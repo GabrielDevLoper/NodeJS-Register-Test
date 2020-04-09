d0015cba864c185065961f82c3ae872c912376f5
Gabriel <gabriel.devloper@gmail.com>
2020-04-09 19:25:03 -0300

    variaveis ambientes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://hpgabriel:hp18360515@cluster0-ohklh.mongodb.net/api-node?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
